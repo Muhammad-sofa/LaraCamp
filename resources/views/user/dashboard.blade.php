43a6c85fef1772379ba7180c279eedca23b263f8
Sofa <muhammadsofa52@yahoo.com>
2024-07-26 14:57:37 +0700

    Admin Integration Dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,61 +1,53 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="dashboard my-5">
-    <div class="container">
-        <div class="row text-left">
-            <div class=" col-lg-12 col-12 header-wrap mt-4">
-                <p class="story">
-                    DASHBOARD
-                </p>
-                <h2 class="primary-header ">
-                    My Bootcamps
-                </h2>
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2">
+            <div class="card-header">
+                My Camps
+            </div>
+            <div class="card-body">
+                @include('components.alert')
+                <table class="table">
+                    <thead>
+                        <th>User</th>
+                        <th>Camp</th>
+                        <th>Price</th>
+                        <th>Register Data</th>
+                        <th>Paid Status</th>
+                        <th>Action</th>
+                    </thead>
+                    <tbody>
+                        @forelse($checkouts as $checkout)
+                        <tr>
+                            <td>{{ $checkout->User->name }}</td>
+                            <td>{{ $checkout->Camp->title }}</td>
+                            <td>{{ $checkout->Camp->price }}</td>
+                            <td>{{ $checkout->Camp->created_at->format('M d Y') }}</td>
+                            <td>
+                                @if($checkout->is_paid)
+                                <span class="badge bg-success">Paid</span>
+                                @else
+                                <span class="badge bg-warning">Waiting</span>
+                                @endif
+                            </td>
+                            <td>
+                                <form action="" method="post">
+                                    @csrf
+                                    <button class="btn btn-primary btn-sm">Set to Paid</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td colspan="3">No. Camps Registered</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="row my-5">
-            @include('components.alert')
-            <table class="table">
-                <tbody>
-                    @forelse ($checkouts as $checkout)
-                    <tr class="align-middle">
-                        <td width="18%">
-                            <img src="{{ asset('images/item_bootcamp.png') }}" height="120" alt="">
-                        </td>
-                        <td>
-                            <p class="mb-2">
-                                <strong>{{ $checkout->Camp->title }}</strong>
-                            </p>
-                            <p>
-                                {{ $checkout->created_at->format('M d, Y') }}
-                            </p>
-                        </td>
-                        <td>
-                            <strong>${{ $checkout->Camp->price }}</strong>
-                        </td>
-                        <td>
-                            @if ($checkout->is_paid)
-                            <strong class="text-success">Payment Success</strong>
-                            @else
-                            <strong>Waiting for Payment</strong>
-                            @endif
-                        </td>
-                        <td>
-                            <a href="https://wa.me/6281234567890?Hi, I Want to ask about class {{ $checkout->Camp->title }}" class="btn btn-primary">
-                                Contact Support
-                            </a>
-                        </td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <td colspan="5">
-                            <h3>No Data</h3>
-                        </td> 
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
     </div>
-</section>
+</div>
 @endsection
