8273729dfc5c6913e4c3918a4bfd8864738674af
Sofa <muhammadsofa52@yahoo.com>
2024-08-05 10:32:00 +0700

    Discount Store Discount

--- app/Http/Controllers/Admin/DiscountController.php
+++ app/Http/Controllers/Admin/DiscountController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Discount\Store;
+use App\Models\Discount;
 
 class DiscountController extends Controller
 {
@@ -15,5 +17,12 @@ class DiscountController extends Controller
     public function create()
     {
         return view('admin.discount.create');
+    }
+
+    public function store(Store $request)
+    {
+        $discount = Discount::create($request->all());
+        $request->session()->flash('success', 'A new discount has been created');
+        return redirect(route('admin.discount.index'));
     }
 }
