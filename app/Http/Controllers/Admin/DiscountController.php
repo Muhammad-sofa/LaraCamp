13b39c17eba94a71b31af293c9729960b6a53ccd
Sofa <muhammadsofa52@yahoo.com>
2024-08-05 13:30:16 +0700

    Discount Update Discount

--- app/Http/Controllers/Admin/DiscountController.php
+++ app/Http/Controllers/Admin/DiscountController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Discount\Store;
+use App\Http\Requests\Admin\Discount\Update;
 use App\Models\Discount;
 
 class DiscountController extends Controller
@@ -26,6 +27,20 @@ class DiscountController extends Controller
     {
         $discount = Discount::create($request->all());
         $request->session()->flash('success', 'A new discount has been created');
+        return redirect(route('admin.discount.index'));
+    }
+
+    public function edit (Discount $discount)
+    {
+        return view('admin.discount.edit', [
+            'discount' => $discount
+        ]);
+    }
+
+    public function update(Update $request, Discount $discount)
+    {
+        $discount->update($request->all());
+        $request->session()->flash('success', "Discount {$discount->name} has been updated");
         return redirect(route('admin.discount.index'));
     }
 }
